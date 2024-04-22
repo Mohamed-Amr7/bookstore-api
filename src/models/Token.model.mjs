17b6f903d3e3923532461dac66a64eed6e96b1f9
Mohamed-Amr7 <moh.amr777@gmail.com>
2024-04-23 00:55:30 +0200

    Add Token schema with validation and plugins

---
+++ src/models/Token.model.mjs
@@ -0,0 +1,44 @@
+import mongoose from "mongoose";
+import {toJSON} from "./plugins/index.mjs";
+import {tokenTypes} from "../config/tokens.mjs";
+
+const tokenSchema = mongoose.Schema(
+    {
+        token: {
+            type: String,
+            required: true,
+            index: true,
+        },
+        user: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User',
+            required: true,
+        },
+        type: {
+            type: String,
+            enum: [tokenTypes.REFRESH, tokenTypes.RESET_PASSWORD, tokenTypes.VERIFY_EMAIL],
+            required: true,
+        },
+        expires: {
+            type: Date,
+            required: true,
+        },
+        blacklisted: {
+            type: Boolean,
+            default: false,
+        },
+    },
+    {
+        timestamps: true,
+    }
+);
+
+// add plugin that converts mongoose to json
+tokenSchema.plugin(toJSON);
+
+/**
+ * @typedef Token
+ */
+const Token = mongoose.model('Token', tokenSchema);
+
+export default Token;
