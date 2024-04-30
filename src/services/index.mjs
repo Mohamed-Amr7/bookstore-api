36646526f88dfefe7a1a7c508e3cbf7757ca1498
Mohamed-Amr7 <moh.amr777@gmail.com>
2024-05-01 01:21:11 +0300

    Add book service

--- src/services/index.mjs
+++ src/services/index.mjs
@@ -1,9 +1,11 @@
 import authService from './auth.service.mjs'
 import tokenService from './token.service.mjs'
 import userService from './user.service.mjs'
+import bookService from './book.service.mjs'
 
 export {
     authService,
     tokenService,
-    userService
+    userService,
+    bookService,
 }
