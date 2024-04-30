a5e98e94c5086803d0cf793dc7c0981f347ae034
Mohamed-Amr7 <moh.amr777@gmail.com>
2024-05-01 01:11:15 +0300

    Add isAdmin middleware

---
+++ src/middlewares/auth.mjs
@@ -0,0 +1,31 @@
+import passport from "passport";
+import ApiError from "../utils/ApiError.mjs";
+import httpStatus from "http-status";
+import {ROLES} from "../constants/roles.mjs";
+import logger from "../config/logger.mjs";
+
+export const isLoggedIn = (req, res, next) => {
+    passport.authenticate('jwt', {session: false}, (err, user, info) => {
+        if (err) {
+            return next(err);
+        }
+
+        if (!user) {
+            throw new ApiError(httpStatus.UNAUTHORIZED, 'Please authenticate')
+        }
+
+        req.user = user;
+        next();
+    })(req, res, next);
+};
+
+export const isAdmin = (req, res, next) => {
+    logger.debug(JSON.stringify(req.user.role))
+    if (!req.user?.role) {
+        throw new ApiError(httpStatus.UNAUTHORIZED, 'Please Authenticate');
+    }
+    if (req.user.role !== ROLES.ADMIN) {
+        throw new ApiError(httpStatus.FORBIDDEN, 'You are not authorized to perform this action. Admin privileges required.');
+    }
+    next();
+}
