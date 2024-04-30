16f77cd122072e834b135f92e867aeecc0fd0aca
Mohamed-Amr7 <moh.amr777@gmail.com>
2024-05-01 01:24:53 +0300

    Remove express-rbac

--- src/app.mjs
+++ src/app.mjs
@@ -5,7 +5,6 @@ import mongoSanitize from 'express-mongo-sanitize'
 import compression from 'compression'
 import cors from 'cors'
 import passport from 'passport'
-import auth from 'express-rbac'
 import httpStatus from 'http-status'
 import config from './config/config.mjs'
 import morgan from './config/morgan.mjs'
@@ -15,7 +14,6 @@ import ApiError from './utils/ApiError.mjs'
 import {authLimiter} from "./middlewares/rateLimiter.mjs";
 import {jwtStrategy} from "./config/passport.mjs";
 import {errorConverter, errorHandler} from './middlewares/error.mjs'
-import {ROLES} from "./constants/roles.mjs";
 
 const app = express();
 
@@ -50,16 +48,6 @@ app.options('*', cors());
 app.use(passport.initialize());
 passport.use(jwtStrategy)
 
-// Role-Based Access Control middleware for authorization
-app.use(auth.authorize({
-        bindToProperty: 'user'
-    }, (req, done) => {
-        let auth = {
-            roles: Object.values(ROLES),
-        };
-        done(auth);
-    })
-)
 
 // limit repeated failed requests to auth endpoints
 if (config.env === 'production') {
