b86b194838db2bcd414ae1c9018eab8597b5d7cd
Mohamed-Amr7 <moh.amr777@gmail.com>
2024-05-05 23:59:41 +0300

    Add Cart and Order models

--- src/models/index.mjs
+++ src/models/index.mjs
@@ -1,7 +1,10 @@
-import Token from "./Token.model.mjs";
-import User from "./User.model.mjs";
-import Book from "./Book.model.mjs";
+import Token from "./Token.model.mjs"
+import User from "./User.model.mjs"
+import Book from "./Book.model.mjs"
+import Cart from "./Cart.model.mjs"
+import Order from "./Order.model.mjs"
+
 
 export {
-    Token, User, Book
+    Token, User, Book, Cart, Order
 }
