e8212cd76b0cef86c963a5c836e41125547c7b5c
Mohamed-Amr7 <moh.amr777@gmail.com>
2024-05-11 18:24:45 +0300

    Enhance Order Operations: Implement Stock Updates in Add/Delete orders, Handled Concurrency in Add Orders

--- src/services/order.service.mjs
+++ src/services/order.service.mjs
@@ -1,6 +1,10 @@
 import httpStatus from "http-status"
 import ApiError from "../utils/ApiError.mjs"
-import {Order} from "../models/index.mjs"
+import {Book, Order} from "../models/index.mjs"
+import {bookService} from "./index.mjs";
+import logger from "../config/logger.mjs";
+
+const MAX_RETRY_COUNT = 1
 
 const queryOrders = async (userId, query) => {
     const {page = 1, limit = 10, ...filters} = query
@@ -30,7 +34,7 @@ const queryOrders = async (userId, query) => {
 const getOrder = async (userId, id) => {
     let order
     try {
-        order = await Order.findOne({_id: id, user: userId});
+        order = await (await Order.findOne({_id: id, user: userId})).populate('items.book');
     } catch (error) {
         throw new ApiError(httpStatus.BAD_REQUEST, "Error Fetching Order")
     }
@@ -42,39 +46,90 @@ const getOrder = async (userId, id) => {
 
 const addOrder = async (userId, orderDetails) => {
     let order
+    let updatedBooks = []
     try {
-        order = await Order.create({
+        order = await (await Order.create({
             user: userId,
             items: orderDetails.items,
             paymentMethod: orderDetails.paymentMethod,
             ...(orderDetails.shippingAddress && {shippingAddress: orderDetails.shippingAddress}),
             ...(orderDetails.contactNumber && {contactNumber: orderDetails.contactNumber}),
             ...(orderDetails.additionalDetails && {additionalDetails: orderDetails.additionalDetails})
-        })
+        })).populate('items.book');
     } catch (error) {
         throw new ApiError(httpStatus.BAD_REQUEST, "Failed to add order", true, error.message);
     }
     if (!order.shippingAddress) {
-        await Order.findByIdAndDelete(order._id)
+        await Order.findByIdAndDelete(order._id);
         throw new ApiError(httpStatus.BAD_REQUEST, "Please provide a shipping address");
+    }
+    for (const item of order.items) {
+        let retryCount = 0;
+        let isRetryRequired = true;
+        let lastError;
+
+        while (isRetryRequired && retryCount < MAX_RETRY_COUNT) {
+            try {
+                const updatedBook = await Book.findOneAndUpdate(
+                    {_id: item.book, stock: {$gte: item.quantity}},
+                    {$inc: {stock: -item.quantity, __v: 1}, $set: {updatedAt: new Date()}},
+                    {new: true}
+                )
+
+                if (!updatedBook) {
+                    await Order.findByIdAndDelete(order._id)
+                    await bookService.revertStockUpdates(updatedBooks)
+                    throw new ApiError(httpStatus.BAD_REQUEST, "Insufficient stock for one or more items")
+                }
+
+                updatedBooks.push({bookId: item.book._id, quantity: item.quantity})
+
+                if (item.book.__v + 1 !== updatedBook.__v) {
+                    throw new ApiError(httpStatus.CONFLICT, "Concurrency conflict occurred while updating book stock")
+                }
+                isRetryRequired = false;
+            } catch (error) {
+                lastError = error
+                retryCount++
+            }
+        }
+
+        if (isRetryRequired) {
+            // Retry count exceeded, handle error
+            await Order.findByIdAndDelete(order._id);
+            await bookService.revertStockUpdates(updatedBooks);
+            throw new ApiError(lastError.statusCode, lastError.message, true, lastError.stack);
+        }
     }
 
     return order;
 }
 
 const deleteOrder = async (userId, id) => {
-    let order
+    let order;
     try {
-        order = await Order.findOne({_id: id, user: userId});
+        order = await Order.findOne({_id: id, user: userId}).populate('items.book');
     } catch (error) {
-        throw new ApiError(httpStatus.BAD_REQUEST, "Error deleting order")
+        throw new ApiError(httpStatus.BAD_REQUEST, "Error deleting order");
     }
+
     if (!order) {
         throw new ApiError(httpStatus.NOT_FOUND, 'Order not found');
     }
+
+    try {
+        for (const item of order.items) {
+            await Book.findByIdAndUpdate(item.book._id, {$inc: {stock: item.quantity}});
+        }
+    } catch (error) {
+        logger.error(`Error updating book stock while deleting order: ${error.message}`);
+    }
+
     await Order.findByIdAndDelete(id);
+
     return order;
 }
+
 
 const updateOrderStatus = async (id, status) => {
     let order
