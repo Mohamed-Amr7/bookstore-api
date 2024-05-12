1e02e5e54097084eb4cf99efc4d90ff69f51a4e0
Mohamed-Amr7 <moh.amr777@gmail.com>
2024-05-13 02:25:38 +0300

    Add phone number validation

--- src/validations/custom.validation.mjs
+++ src/validations/custom.validation.mjs
@@ -1,16 +1,26 @@
+import {parsePhoneNumberFromString} from 'libphonenumber-js'
+
 export const objectId = (value, helpers) => {
     if (!value.match(/^[0-9a-fA-F]{24}$/)) {
-        return helpers.message('"{{#label}}" must be a valid mongo id');
+        return helpers.message('"{{#label}}" must be a valid mongo id')
     }
-    return value;
+    return value
 }
 
 export const password = (value, helpers) => {
     if (value.length < 8) {
-        return helpers.message('password must be at least 8 characters');
+        return helpers.message('password must be at least 8 characters')
     }
     if (!value.match(/\d/) || !value.match(/[a-zA-Z]/)) {
-        return helpers.message('password must contain at least 1 letter and 1 number');
+        return helpers.message('password must contain at least 1 letter and 1 number')
     }
     return value;
 }
+
+export const phoneNumber = (value, helpers) => {
+    const phoneNumber = parsePhoneNumberFromString(value)
+    if (!phoneNumber || !phoneNumber.isValid()) {
+        return helpers.message({custom: 'Invalid phone number'})
+    }
+    return value
+};
