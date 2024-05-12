63fb4ea63f2aee91bed7eac00be9e9142ed65271
Mohamed-Amr7 <moh.amr777@gmail.com>
2024-05-12 17:28:13 +0300

    Implement email-related routes

--- src/routes/v1/auth.route.mjs
+++ src/routes/v1/auth.route.mjs
@@ -2,6 +2,7 @@ import express from "express"
 import validate from "../../middlewares/validate.mjs"
 import {authValidation} from "../../validations/index.mjs"
 import {authController} from "../../controllers/index.mjs"
+import {isLoggedIn} from "../../middlewares/auth.mjs";
 
 const router = express.Router()
 
@@ -9,5 +10,9 @@ router.post('/register', validate(authValidation.register), authController.regis
 router.post('/login', validate(authValidation.login), authController.login)
 router.post('/logout', authController.logout)
 router.post('/refresh-tokens', authController.refreshTokens)
+router.post('/forgot-password', validate(authValidation.forgotPassword), authController.forgotPassword)
+router.post('/reset-password', validate(authValidation.resetPassword), authController.resetPassword)
+router.post('/send-verification-email', isLoggedIn, authController.sendVerificationEmail)
+router.post('/verify-email', validate(authValidation.verifyEmail), authController.verifyEmail)
 
 export default router
