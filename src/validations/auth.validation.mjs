ca0f772ca829e07700d11429eac8f7f7798f15c5
Mohamed-Amr7 <moh.amr777@gmail.com>
2024-05-12 17:24:28 +0300

    Implement validation for auth routes requiring email-related tokens.

--- src/validations/auth.validation.mjs
+++ src/validations/auth.validation.mjs
@@ -22,8 +22,33 @@ export const refreshTokens = {
     }),
 };
 
+export const forgotPassword = {
+    body: Joi.object().keys({
+        email: Joi.string().email().required(),
+    }),
+
+};
+
+export const resetPassword = {
+    query: Joi.object().keys({
+        token: Joi.string().required(),
+    }),
+    body: Joi.object().keys({
+        password: Joi.string().required().custom(password),
+    }),
+};
+
+export const verifyEmail = {
+    query: Joi.object().keys({
+        token: Joi.string().required(),
+    }),
+};
+
 export default {
     register,
     login,
     refreshTokens,
+    forgotPassword,
+    resetPassword,
+    verifyEmail,
 };
