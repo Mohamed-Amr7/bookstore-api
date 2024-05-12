ffc281cde83005e4b3308dcac616ca89382fc5b6
Mohamed-Amr7 <moh.amr777@gmail.com>
2024-05-12 17:30:26 +0300

    Update auth controller for email routes

--- src/controllers/auth.controller.mjs
+++ src/controllers/auth.controller.mjs
@@ -1,20 +1,19 @@
 import httpStatus from "http-status"
 import catchAsync from "../utils/catchAsync.mjs"
-import {authService, tokenService, userService} from '../services/index.mjs'
+import {authService, emailService, tokenService, userService} from '../services/index.mjs'
 import config from "../config/config.mjs";
 
 const register = catchAsync(async (req, res) => {
     const user = await userService.createUser(req.body)
-    const tokens = await tokenService.generateAuthTokens(user)
-    res.status(httpStatus.CREATED).send({user, tokens})
+    res.status(httpStatus.CREATED).json({message: "User created successfully", data: {"user": user}})
 })
 
 const login = catchAsync(async (req, res) => {
     const {email, password} = req.body;
     const user = await authService.loginUserWithEmailAndPassword(email, password);
-    const {access, refresh} = await tokenService.generateAuthTokens(user);
+    const {access: accessToken, refresh} = await tokenService.generateAuthTokens(user);
     res.cookie('jwt', refresh.token, config.jwt.refreshCookieOptions);
-    res.status(httpStatus.OK).send({message: "Login successful", user, access});
+    res.status(httpStatus.OK).json({message: "Login successful", data: {user, accessToken}});
 
 });
 
@@ -29,17 +28,42 @@ const logout = catchAsync(async (req, res) => {
 });
 
 const refreshTokens = catchAsync(async (req, res) => {
-        const {access, refresh} = await authService.refreshAuth(req.cookies.jwt)
-        res.cookie('jwt', refresh.token, config.jwt.refreshCookieOptions)
-        res.send({access});
-    })
-;
+    const {access, refresh} = await authService.refreshAuth(req.cookies.jwt)
+    res.cookie('jwt', refresh.token, config.jwt.refreshCookieOptions)
+    res.send({access});
+});
+
+const forgotPassword = catchAsync(async (req, res) => {
+    const resetPasswordToken = await tokenService.generateResetPasswordToken(req.body.email);
+    await emailService.sendResetPasswordEmail(req.body.email, resetPasswordToken);
+    res.status(httpStatus.NO_CONTENT).send();
+});
+
+const resetPassword = catchAsync(async (req, res) => {
+    await authService.resetPassword(req.query.token, req.body.password);
+    res.status(httpStatus.OK).json({message: 'Password reset successful'});
+});
+
+const sendVerificationEmail = catchAsync(async (req, res) => {
+    const verifyEmailToken = await tokenService.generateVerifyEmailToken(req.user);
+    await emailService.sendVerificationEmail(req.user.email, verifyEmailToken);
+    res.status(httpStatus.NO_CONTENT).send();
+});
+
+const verifyEmail = catchAsync(async (req, res) => {
+    await authService.verifyEmail(req.query.token);
+    res.status(httpStatus.OK).json({message: 'Email verified successfully'});
+});
 
 const authController = {
     register,
     login,
     logout,
     refreshTokens,
+    forgotPassword,
+    resetPassword,
+    sendVerificationEmail,
+    verifyEmail,
 };
 
 export default authController
